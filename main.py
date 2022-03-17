d5a4ad36704eb10f3bc8339f10c67edd159df85f
Jade <82993424+jjadeb@users.noreply.github.com>
2022-03-17 22:37:47 +0000

    finished day 1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 print("Arr Matey! Welcome to the Pirate Name Generator!")
 cereal = input("What is the name of your favourite cereal?\n")
 pet = input("What is the name of a pet you know?\n")
-print("Intoducing Captain " + cereal + " and their First Mate, " + pet + "!")
+print("Intoducing Captain " + cereal + " and their First Mate, " + pet + "!") 
+
